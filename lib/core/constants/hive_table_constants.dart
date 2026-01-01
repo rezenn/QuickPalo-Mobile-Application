e7080a7230521d9a21ad0ed8e4bb40dc6af1caa1
rezen <rezen.khadgi@gmail.com>
2026-01-01 12:09:53 +0545

    local database and errors create

---
+++ lib/core/constants/hive_table_constants.dart
@@ -0,0 +1,14 @@
+class HiveTableConstants {
+  HiveTableConstants._();
+
+  static const String dbName = "quickpalo_db";
+
+  static const int authTypeId = 0;
+  static const String authTable = "auth_table";
+
+  static const int organizationTypeId = 1;
+  static const String organizationTable = "organization_table";
+
+  static const int appointmentTypeId = 2;
+  static const String appointmentTable = "appointment_table";
+}
